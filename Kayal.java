65450ef145a915675129289507f8550094a84fc4
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-22 20:11:39 +0530

    Create Kayal.java

---
+++ Kayal.java
@@ -0,0 +1,16 @@
+class Kayal{
+	public static void main(String args[]){
+    int x=1;
+	while(x<=5){
+		int y=1;
+		while(y<=5){
+			System.out.print(y);
+			System.out.print(" ");
+			y++;
+		}
+	   
+	    x++;
+	}
+	
+}
+}
