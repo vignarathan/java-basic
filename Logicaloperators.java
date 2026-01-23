d4303e50a976144d6f7d87114e9a5bef26a6fa98
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-23 20:26:41 +0530

    Create Logicaloperators.java

---
+++ Logicaloperators.java
@@ -0,0 +1,10 @@
+class Logicaloperators{
+	public static void main(String args[]){
+		boolean a=true;
+		boolean b=false;
+		
+		/*And*/ System.out.println("a && b = "+(a && b));//false
+		/*Or*/ System.out.println("a || b = "+(a || b));//true
+		/*Not*/ System.out.println("!a = "+(!a));//false
+	}
+}
