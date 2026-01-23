2818a06bf846791b876a66db2a8e4a4a733f024c
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-23 20:26:48 +0530

    Create Incrementoperators.java

---
+++ Incrementoperators.java
@@ -0,0 +1,47 @@
+class Incrementoperators{
+	public static void main(String args[]){
+	int x=10;
+	
+	System.out.println(x);//10
+	
+	x =x+1;
+	System.out.println(x);//11
+	
+	x +=1;
+	System.out.println(x);//12
+	
+	//post increment
+	x++;
+	System.out.println(x);//13
+	
+	//pre increment
+	++x;
+	System.out.println(x);//14
+	
+	System.out.println(++x);//15//pre increment
+	System.out.println(x++);//15//post increment
+	System.out.println(x);//16
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
