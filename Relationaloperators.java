f440168809476564323c85705c8f5b4d651c51d6
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-23 20:26:26 +0530

    Create Relationaloperators.java

---
+++ Relationaloperators.java
@@ -0,0 +1,13 @@
+class Relationaloperators{
+	public static void main(String args[]){
+		int x=10;
+		int y=5;
+		
+		System.out.println("x == y = "+(x==y));//false
+		System.out.println("x != y = "+(x!=y));//true
+		System.out.println("x > y = "+(x>y));//true
+		System.out.println("x >= y = "+(x>=y));//true
+		System.out.println("x < y = "+(x<=y));//false
+		System.out.println("x <= y = "+(x<=y));//false
+	}
+}
