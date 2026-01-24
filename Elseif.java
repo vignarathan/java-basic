1424ac4586e1e08add51777c6516de92e5fb099d
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-24 18:30:53 +0530

    Create Elseif.java

---
+++ Elseif.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+class Elseif{
+	public static void main(String args[]){
+		int marks=0;
+		Scanner scan=new Scanner(System.in);
+		System.out.println("*******************************");
+		System.out.print("Enter Your Marks : ");
+		marks=scan.nextInt();
+		System.out.println("*******************************");
+		if(marks>=75){
+			System.out.println("A");
+		}else if(marks>=65){
+			System.out.println("B");
+		}else if(marks>=55){
+			System.out.println("C");
+		}else if(marks>=45){
+			System.out.println("S");
+		}else{
+			System.out.println("F");
+			}
+				
+	System.out.println("*******************************");
+}
+}
