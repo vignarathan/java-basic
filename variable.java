1880417ecc9378abbe9b5a6572b98dcc4ac6bdc5
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-26 21:16:08 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,73 @@
+class Variable{
+ public static void main(String args[]){
+	 
+//Examples for Variables
+
+  boolean bool=false;
+  System.out.println(bool);
+
+  byte by;
+  by=120;
+  System.out.println("byte:"+by);
+ 
+  short sh;
+  sh=32000;
+  System.out.println("short:"+sh);
+
+  int in;
+  in=2500000;
+  System.out.println("int:"+in);
+ 
+  long lg;
+  lg=45786451269L;
+  System.out.println("long:"+lg);
+
+  float fl;
+  fl=3.4F;
+  System.out.println("float:"+fl);
+  
+  double db;
+  db=67.786D;
+  System.out.println("double:"+db);
+  
+  double d;
+  d=67.786;
+  System.out.println("double:"+d);
+  
+  byte ch='A';
+  System.out.println("Char:"+ch);
+  
+  byte ch1=65;
+  System.out.println("Char:"+ch1);
+  
+  byte ch2=0101;
+  System.out.println("Char:"+ch2);
+  
+  byte ch3=0x41;
+  System.out.println("Char:"+ch3);
+  
+  byte ch4='\u0041';
+  System.out.println("Char:"+ch4);
+  
+  byte ch5=0b1000001;
+  System.out.println("Char:"+ch5);
+  
+  char ch6='A';
+  System.out.println("Char:"+ch6);
+  
+  char ch7=65;
+  System.out.println("Char:"+ch7);
+  
+  char ch8=0101;
+  System.out.println("Char:"+ch8);
+  
+  char ch9=0x41;
+  System.out.println("Char:"+ch9);
+  
+  char ch10='\u0041';
+  System.out.println("Char:"+ch10);
+  
+  char ch11=0b1000001;
+  System.out.println("Char:"+ch11);
+}
+}
