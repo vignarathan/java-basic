9819889979ba5147c8c37d2d75ef78c270277ee8
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-26 21:21:22 +0530

    Create Nested6.java

---
+++ Nested6.java
@@ -0,0 +1,27 @@
+class Nested6{
+	public static void main(String args[]){
+	int x=1;
+	while(x<=5){
+		int y=1;
+		while(y<=x){
+			 System.out.print(y);
+			 y++;
+		}
+	    System.out.println(" ");
+	    x++;
+	}
+	System.out.println("-----------------");
+	int a=1;
+	while(a<=5){
+		int b=1;
+		while(b<=a){
+			 System.out.print(a);
+			 b++;
+		}
+	    System.out.println(" ");
+	    a++;
+	}
+	
+			
+	}
+}
