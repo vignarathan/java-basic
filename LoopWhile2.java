a9345a4f0058cb158af4725c40ed7a432dc740f5
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-26 21:18:22 +0530

    Create LoopWhile2.java

---
+++ LoopWhile2.java
@@ -0,0 +1,20 @@
+class LoopWhile2{
+	public static void main(String args[]){
+		int x=1;
+		int y=1;
+		while(y<=15){
+			System.out.println(y);
+			x++;
+			y=y+x;
+		}
+        System.out.println("****************");	
+		int a=1;
+		int b=1;
+		while(b<=25){
+			System.out.println(b);
+			a=a+1;
+			b=a*a;
+		}
+	}
+}
+	
