138619aa3f926c5a98c584690634443edab90298
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-27 21:58:59 +0530

    Create Nestedw2.java

---
+++ Nestedw2.java
@@ -0,0 +1,14 @@
+class Nestedw2{
+	public static void main(String args[]){
+		int x=1;
+		while(x<=5){
+			int y=1;
+			while(y<=x){
+				System.out.print(y);
+				y++;
+			}
+		System.out.println("");
+		x++;
+		}
+	}
+}
