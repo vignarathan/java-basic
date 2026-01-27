87aefd4da51bc5c329a9b259a6c4b01b1595b8b8
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-27 21:58:55 +0530

    Create Nestedw1.java

---
+++ Nestedw1.java
@@ -0,0 +1,23 @@
+class Nestedw1{
+	public static void main(String args[]){
+		int x=1;
+		while(x<=5){
+			int y=1;
+			while(y<=3){
+				System.out.print(y);
+				y++;
+			}
+			System.out.println(x);
+			x++;
+			//System.out.println("");-no need
+		}
+		
+	
+System.out.println(x);
+System.out.print(x);
+System.out.println(x);
+System.out.print(x);
+System.out.println(x);	
+		
+	}
+}
