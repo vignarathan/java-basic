c091ae57294376e7f6cb1f54eb8e2257d052d232
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-27 21:58:44 +0530

    Create Nestedf1.java

---
+++ Nestedf1.java
@@ -0,0 +1,10 @@
+class Nestedf1{
+	public static void main(String args[]){
+		for(int x=1;x<=5;x++){
+			for(int y=1;y<=3;y++){
+				System.out.print(y);
+			}
+			System.out.println(x);
+		}
+	}
+}
