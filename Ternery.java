975c0deb61028f71d644e5533e6ece0291fdf01b
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-27 21:59:28 +0530

    Create Ternery.java

---
+++ Ternery.java
@@ -0,0 +1,17 @@
+class Ternery{
+	public static void main(String args[]){
+		
+		//method1
+		int marks=78;
+		String result=(marks>=75)?"PASS":"FAIL";
+		   System.out.println(result);
+		   
+		System.out.println("***************");
+		
+		//method2
+		int marks1=43;
+		String result1;
+		result1=(marks1>=45)?"PASS":"FAIL";
+		   System.out.println(result1);
+		}
+   }
