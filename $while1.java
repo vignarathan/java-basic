b2605754d4fd5d009579bc4c307cb225df065296
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-27 21:58:03 +0530

    Create $while1.java

---
+++ $while1.java
@@ -0,0 +1,18 @@
+class $while1{
+	public static void main(String args[]){
+		int x=1;
+		while(x<=10){
+		System.out.print(x);
+		x++;
+		}
+		
+System.out.println(" ");
+	
+	    int y=1;
+		while(y<=10){
+			System.out.print(y);
+			System.out.print(" ");
+			y++;
+		}
+	}
+}
