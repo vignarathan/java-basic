a3dc2afe8c858ecf13cb8c5defc56599fdf31632
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-27 21:59:03 +0530

    Create Nestedw3.java

---
+++ Nestedw3.java
@@ -0,0 +1,14 @@
+class Nestedw3{
+	public static void main(String args[]){
+		int x=5;
+		while(x>0){
+			int y=1;
+			while(y<=x){
+				System.out.print(y);
+				y++;
+			}
+			System.out.println("");
+			x--;
+		}
+	}
+}
