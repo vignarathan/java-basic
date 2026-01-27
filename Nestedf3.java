3a9338b3ff049883ae815d0fc742120fcc3fd1de
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-27 21:58:52 +0530

    Create Nestedf3.java

---
+++ Nestedf3.java
@@ -0,0 +1,11 @@
+class Nestedf3{
+	public static void main(String args[]){
+		for(int x=5;x>0;x--){
+			for(int y=1;y<=x;y++){
+				System.out.print(y);
+			}
+		    System.out.println("");
+		}
+	}
+}
+			
