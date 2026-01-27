51747936c385ae32ead287242c49abfbeec2ba20
S.Vignarathan <s.vignarathan@gmail.com>
2026-01-27 21:59:23 +0530

    Create second.java

---
+++ second.java
@@ -0,0 +1,12 @@
+class Secondclass{     
+      public static void main (String args[]){
+		  System.out.println("************************");
+		  System.out.println("My Info");
+		  System.out.println("************************");
+		  System.out.println("First name:Vignarathan");
+		  
+		  System.out.println("Last name :Rathan");
+		  System.out.println("************************");
+		  
+	  }
+}
