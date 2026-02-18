4c75ee10ad9bf0a151a463701b70c61ebfd81c6a
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-18 14:06:48 +0530

    Create Array4.java

---
+++ Array/Array4.java
@@ -0,0 +1,10 @@
+class Array4{
+	public static void main(String args[]){
+		int[] x={85,69,45,23,78,96,12};
+		int y=0;
+		do{
+			System.out.println(x[y]);
+			y++;
+		}while(y<=x.length-1);
+	}
+}
