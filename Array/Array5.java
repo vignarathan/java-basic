7b9ea58e2561d81e6c0d2e49e2e0be5c7badb19d
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-18 14:06:50 +0530

    Create Array5.java

---
+++ Array/Array5.java
@@ -0,0 +1,10 @@
+class Array5{
+	public static void main(String args[]){
+		String[] x={"maths","tamil","science","english","religion"};
+		
+		for(int y=x.length-1;y>=0;y=y-1){
+			System.out.println(x[y]);
+		}
+	}
+}
+	
