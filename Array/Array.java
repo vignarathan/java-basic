18cd1e80d30611eef5b3872fffbbb82552bbaced
S.Vignarathan <s.vignarathan@gmail.com>
2026-02-18 14:06:43 +0530

    Create Array.java

---
+++ Array/Array.java
@@ -0,0 +1,18 @@
+class Array{
+	public static void main(String args[]){
+		int[] marks;
+		marks=new int[5];
+		marks[0]=54;
+		marks[1]=96;
+		marks[2]=65;
+		marks[3]=56;
+		marks[4]=69;
+		
+		System.out.println(marks[4]);
+		System.out.println(marks[0]);
+		System.out.println(marks[1]);
+		
+	}
+	
+}
+	
