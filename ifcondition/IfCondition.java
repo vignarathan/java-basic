62221d012fdef20ae75486b23cb714787e80c41c
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:20:09 +0530

    Create IfCondition.java

---
+++ ifcondition/IfCondition.java
@@ -0,0 +1,31 @@
+class IfCondition{
+
+     public static void main(String args[]){
+		 int x=52;
+
+        System.out.println("line 1");
+		if (x==10){
+		    System.out.println("line 2");
+		}else{
+			System.out.println("line 3");
+		}
+ System.out.println("*****************************************");	
+
+        System.out.println("line 1");
+		 if(!true){
+		     System.out.println("line 2");
+		 }else{
+		     System.out.println("line 3");
+		 }
+System.out.println("*****************************************");	  
+		int marks=75;
+		
+		 System.out.println("Your Grade");
+		 if(marks>50){
+			  System.out.println("Pass");
+		 }else{
+			  System.out.println("Fail");
+		     }
+ 
+ }
+}
