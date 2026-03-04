802948b9e958ab0218ad021211a24ac203163873
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:19:25 +0530

    Create ForLoop.java

---
+++ forloop/ForLoop.java
@@ -0,0 +1,13 @@
+class ForLoop{
+	public static void main(String args[]){
+		for(int x=1;x<=5;x++){
+			System.out.println(x);
+		}
+	System.out.println("**********************");
+	int y=1;
+	while(y<=5){
+		System.out.println(y);
+		y++;
+	}
+	}
+}
