089dd203d0f768ae067f508439ffee0812425da8
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:21:29 +0530

    Create Input2.java

---
+++ userinput/Input2.java
@@ -0,0 +1,10 @@
+ import java.util.Scanner;
+ class Input2{
+      public static void main(String args[]){
+		String name="";	
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter Your Name : ");
+		name=scan.next();
+		System.out.println("Your name is : "+name);
+	}
+ }
