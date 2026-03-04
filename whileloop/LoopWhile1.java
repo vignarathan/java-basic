4145145aa7cd1ed318efa07fc9ef8d5558ab7649
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:22:04 +0530

    Create LoopWhile1.java

---
+++ whileloop/LoopWhile1.java
@@ -0,0 +1,33 @@
+class LoopWhile1{
+	public static void main(String args[]){
+		int x=1;
+		while(x<=5){
+			System.out.println(x);
+			x++;
+		}
+		System.out.println("**************");
+		int y=5;
+		while(y>0){
+			System.out.println(y);
+			y=y-1;
+		}
+		System.out.println("**************");
+		int z=1;
+		while(z<=5){
+			System.out.println(z);
+			z=z+2;
+		}
+		System.out.println("**************");
+		int a=2;
+		while(a<=10){
+			System.out.println(a);
+			a=a+2;
+		}
+		System.out.println("**************");
+		int b=5;
+		while(b<=25){
+			System.out.println(b);
+			b=b+5;
+		}	
+	}
+}
