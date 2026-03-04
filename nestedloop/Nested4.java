5c0c6b4c38445cc0bee9342b9a77c07f1a8871fd
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:20:37 +0530

    Create Nested4.java

---
+++ nestedloop/Nested4.java
@@ -0,0 +1,37 @@
+class Nested4{
+	public static void main(String args[]){
+		int x=1;
+		while(x<=5){
+		    if(x>4){
+			  System.out.print(x);
+		    }if(x>3){
+			  System.out.print(x);
+			}if(x>2){
+			  System.out.print(x);
+		    }if(x>1){
+			  System.out.print(x);
+			}if(x>0){
+			  System.out.print(x);
+			}
+			System.out.println("");
+			x++;
+		}
+	System.out.println("________________________");
+	    int y=5;
+		while(y>0){
+			if(y>0){
+				System.out.print(y);
+			}if(y>1){
+				System.out.print(y);
+			}if(y>2){
+				System.out.print(y);
+			}if(y>3){
+				System.out.print(y);
+			}if(y>4){
+				System.out.print(y);
+			}
+			System.out.println("");
+			y=y-1;
+		}
+	}
+}
