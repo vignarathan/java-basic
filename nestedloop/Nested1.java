3856f78a612d12ad814a3dba486cbac4ae8a1ab3
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:20:33 +0530

    Create Nested1.java

---
+++ nestedloop/Nested1.java
@@ -0,0 +1,26 @@
+class Nested1{
+	public static void main(String args[]){
+    int x=1;
+	while(x<=5){
+		int y=1;
+		while(y<=5){
+			System.out.print(y);
+			y++;
+		}
+	    System.out.println(" ");
+	    x++;
+	}
+	System.out.println("****************");
+	int a=1;
+	while(a<=5){
+		int b=1;
+		while(b<=5){
+			System.out.println(b);
+			b++;
+		}
+	    System.out.println(" ");
+	    a++;
+	}
+ }
+}
+		
