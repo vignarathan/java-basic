d0a5b1abbd60235b6595dfd0ffe7dfaaa08fa88b
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:20:44 +0530

    Create Nested7.java

---
+++ nestedloop/Nested7.java
@@ -0,0 +1,14 @@
+class Nested7{
+	public static void main(String args[]){
+	int x=1;
+	while(x<=5){
+		int y=1;
+		while(y<=x){
+			 System.out.print("*");
+			 y++;
+		}
+	    System.out.println(" ");
+	    x++;
+	}
+	}
+}
