b66d376f3d5ffca9b1a1d6c574ec0ca79f41a94d
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:21:13 +0530

    Create pattern1.java

---
+++ pattern1.java
@@ -0,0 +1,12 @@
+class Pattern1{
+	public static void main(String args[]){
+		int x,y;
+		for(x=1;x>0;x--){
+			for(y=0;y<5-x;x++){
+				System.out.print("");
+			}
+			for(y=0;y)
+			System.out.println("");
+		}
+	}
+}
