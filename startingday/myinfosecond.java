1aa7ef6b4d6588885499f7d31d61f4e83c40f8de
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:21:27 +0530

    Create myinfosecond.java

---
+++ startingday/myinfosecond.java
@@ -0,0 +1,17 @@
+class Myinfosecond{
+   public static void main(String args[]){
+   String Fname;
+   Fname="Vignarathan";
+   
+   String Lname;
+   Lname="Sivalingam";
+   
+   System.out.println("*********************************");
+   System.out.println("My Info");
+   System.out.println("*********************************");
+   System.out.println("First name :"+ Fname);
+   System.out.println("Last name  :"+ Lname);
+   System.out.println("*********************************");
+   
+}
+}
