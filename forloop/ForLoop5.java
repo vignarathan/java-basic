314049f277f4c08c8437d5cc8b17f0ea9e9c5eb1
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:19:40 +0530

    Create ForLoop5.java

---
+++ forloop/ForLoop5.java
@@ -0,0 +1,25 @@
+class ForLoop5{
+	public static void main(String args[]){
+		for(int x=1;x<=5;x++){
+			for(int y=1;y<=x;y++){
+				System.out.print(y);
+            }
+        System.out.println("");
+        }
+		System.out.println("--------------");
+		for(int x=1;x<=5;x++){
+			for(int y=1;y<=x;y++){
+				System.out.print(x);
+            }
+        System.out.println("");
+        }
+		System.out.println("--------------");
+		for(int x=1;x<=5;x++){
+			for(int y=1;y<=x;y++){
+				System.out.print("*");
+            }
+        System.out.println("");
+        }
+		
+    }
+}	
