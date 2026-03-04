8aa743f725440426ef7f8d605d4cc9a5ef85cbca
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:20:54 +0530

    Create Assignmentoperators.java

---
+++ operators/Assignmentoperators.java
@@ -0,0 +1,21 @@
+class Assignmentoperators{
+	public static void main(String args[]){
+		int x=10;
+		System.out.println("x= "+x);//10
+		
+		x += 5;
+		System.out.println("x += 5 ="+x);//15
+		
+		x -= 2;
+		System.out.println("x -= 2 ="+x);//13
+		
+		x *= 3;
+		System.out.println("x *= 3 ="+x);//39
+		
+		x /= 4;
+		System.out.println("x /= 4 ="+x);//9
+		
+		x %= 5;
+		System.out.println("x %= 5 ="+x);//4
+	}
+}
