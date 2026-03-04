3e9e1f4c8575d611fe58d1de4fe81837935fd84d
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-04 16:21:24 +0530

    Create myinfo.java

---
+++ startingday/myinfo.java
@@ -0,0 +1,14 @@
+javaclass MyInfo{
+    public static void main(String args[]){
+    System.out.println ("***************************************");
+	System.out.println ("My Info");
+	System.out.println ("***************************************");
+	System.out.println ("First name : Vignarathan");
+	System.out.println ("***************************************");
+	System.out.println ("Last name : Sivalingam");
+	System.out.println ("***************************************");
+	
+	
+	
+	}
+	}
