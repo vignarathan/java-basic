0f298deb1d3476296a58a801b878b50062e7bcb9
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-10 16:59:26 +0530

    Create Mainpen.java

---
+++ Mainpen.java
@@ -0,0 +1,13 @@
+class Mainpen{
+	public static void main(String args[]){
+	
+	Pen obj=new Pen();
+	obj.brand="ten";
+	obj.madein="Srilanka";
+	obj.colour="blue";
+	obj.hight=10.7;
+	obj.weight=53;
+	
+	obj.write();
+    }
+}
