c404afbcf767ac0bb88b93318fffc04112666208
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-10 16:59:23 +0530

    Update Days.java

--- Days.java
+++ Days.java
@@ -1,20 +1,20 @@
 class Days{
 	public static void main(String args[]){
-		String day;
-		day="1";
-		if(day=="1"){
+		int day;
+		day=1;
+		if(day==1){
 			System.out.println("Monday");
-		}else if(day=="2"){
+		}else if(day==2){
 			System.out.println("Tuesday");
-		}else if(day=="3"){
+		}else if(day==3){
 			System.out.println("Wednesday");
-		}else if(day=="4"){
+		}else if(day==4){
 			System.out.println("Thursday");
-		}else if(day=="5"){
+		}else if(day==5){
 			System.out.println("Friday");
-		}else if(day=="6"){
+		}else if(day==6){
 			System.out.println("Saturday");
-		}else if(day=="7"){
+		}else if(day==7){
 			System.out.println("Sunday");
 		}
 	}
