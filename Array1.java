1491ed4f0ca65050778bf12b3e89d5d47d8e5047
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-10 16:59:11 +0530

    Create Array1.java

---
+++ Array1.java
@@ -0,0 +1,13 @@
+class Array1{
+	public static void main(String args[]){
+		int[] x={96,84,65};
+		String[] y={"Maths","Science","Tamil"};
+		
+		
+		System.out.println(y[0]+":"+x[0]);
+		System.out.println(y[2]+":"+x[1]);
+		System.out.println(y[1]+":"+x[2]);
+		
+		
+	}
+}
