cddab359a1da81bd03df06c842c69db49837e82b
S.Vignarathan <s.vignarathan@gmail.com>
2026-03-10 16:59:59 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,13 @@
+class Student{
+	int id;
+	String name;
+	
+	void setstudent(){
+		id=0003;
+		name="Vignarathan";
+	}
+	void getstudent(){
+		System.out.println("Student ID:"+id);
+		System.out.println("Student Name:"+name);
+	}
+}
